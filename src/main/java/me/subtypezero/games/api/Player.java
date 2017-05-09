52dbb1ec2a0a5a5740e4808cb9de0ab3a2684b88
SubtypeZero <subtypezero@gmail.com>
2017-05-08 20:32:10 -0500

    Add Player class

---
+++ src/main/java/me/subtypezero/games/api/Player.java
@@ -0,0 +1,83 @@
+package me.subtypezero.games.api;
+
+public class Player {
+	private final int id;
+	private int balance;
+	private int bet;
+
+	/**
+	 * Create a new player
+	 * @param id the client id
+	 * @param balance the starting balance
+	 */
+	public Player(int id, int balance) {
+		this.id = id;
+		this.balance = balance;
+	}
+
+	/**
+	 * Get the player's id
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * Get the player's balance
+	 * @return the balance
+	 */
+	public int getBalance() {
+		return balance;
+	}
+
+	/**
+	 * Give money to the player
+	 * @param amount the amount to give
+	 */
+	public void giveMoney(int amount) {
+		this.balance += amount;
+	}
+
+	/**
+	 * Take money from the player
+	 * Note: Taking too much will result in negative values
+	 * @param amount the amount to take
+	 */
+	public void takeMoney(int amount) {
+		this.balance -= amount;
+	}
+
+	/**
+	 * Get the player's bet
+	 * @return the bet
+	 */
+	public int getBet() {
+		return bet;
+	}
+
+	/**
+	 * Add to the player's bet
+	 * @param amount the amount to add
+	 */
+	public void addBet(int amount) {
+		this.bet += amount;
+	}
+
+	/**
+	 * Subtract from the player's bet
+	 * Note: Subtracting too much will result in negative values
+	 * @param amount the amount to subtract
+	 */
+	public void subBet(int amount) {
+		this.bet -= amount;
+	}
+
+	/**
+	 * Set the player's bet
+	 * @param amount the amount to set
+	 */
+	public void setBet(int amount) {
+		this.bet = amount;
+	}
+}
