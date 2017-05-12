405a6bd7a2f227703f06afd01a0766939aac829a
SubtypeZero <subtypezero@gmail.com>
2017-05-11 22:28:34 -0500

    Add values to actions and results

--- src/main/java/me/subtypezero/games/api/event/Action.java
+++ src/main/java/me/subtypezero/games/api/event/Action.java
@@ -1,5 +1,15 @@
 package me.subtypezero.games.api.event;
 
 public enum Action {
-	DOUBLE, SPLIT, CLEAR, DEAL, STAND, HIT, REPEAT
+	DOUBLE(1), SPLIT(2), CLEAR(3), DEAL(4), STAND(5), HIT(6), REPEAT(7);
+
+	private int value;
+
+	Action(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
 }
